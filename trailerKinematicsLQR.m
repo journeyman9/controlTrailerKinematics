cc33f4427b1029e3294fcf6ab5a1c4d9afd13e6f
journeyman9 <t4ke4journey@gmail.com>
2018-05-23 23:33:32 -0700

    Determined need to look at robustness of global coordinate transformation
    when looking at simple physics, needed to change [vr; 0] to [0; vr] for example if going up vs. right.

--- trailerKinematicsLQR.m
+++ trailerKinematicsLQR.m
@@ -7,16 +7,19 @@ clear; close all; clc;
 lr = 1.96; %[m] tractor wheelbase
 lt = 4; %[m] trailer wheelbase
 lh = 0.53; %[m] hitch wheelbase
+
+%% Change 
 vr = 1; %[m/s] keep below 4.5 m/s
+ur = deg2rad(-0.5); %[deg/s] max 30
 
 tractorParams = [lr lt lh vr];
 
 %% At steady state, theta = phi_d_r - phi_d_t = 0, so vt_ss = wr*R
-Radius = 6; %[m] Radius of turn
-sigma = 1; % clock wise = -1, counter-clockwise = 1
-wss = sigma*abs(vr)*sqrt(lt.^2 + Radius.^2 - lh^2)/(lt.^2 + Radius.^2 - lh.^2);
-thss = -2*sigma*sign(vr)*atan((Radius - sqrt(lt.^2 + Radius.^2 + lh.^2))/(lt -lh));
-vt = vr*cos(thss) + lh*wss*sin(thss);
+% Radius = 6; %[m] Radius of turn
+% sigma = 1; % clock wise = -1, counter-clockwise = 1
+% wss = sigma*abs(vr)*sqrt(lt.^2 + Radius.^2 - lh^2)/(lt.^2 + Radius.^2 - lh.^2);
+% thss = -2*sigma*sign(vr)*atan((Radius - sqrt(lt.^2 + Radius.^2 + lh.^2))/(lt -lh));
+% vt = vr*cos(thss) + lh*wss*sin(thss);
 
 %% Linearized State Space
 A = [0          1           0           0;
@@ -70,22 +73,15 @@ Bbar = B;
 [K S e] = lqr(sys, QQ, RR, NN);
 [est, L, P] = kalman(ss(A, [B Bbar], C, zeros(2,2)), QN, RN);
 
-%% Set Point Control
-Q_sp = [A, B; G, H];
-[n, n] = size(A);
-% [l, p] = size(C); %Number of controlled outputs
-l = 2;
-m = 1; %Number of process inputs, or just inputs
-M = pinv(Q_sp); %psuedo inverse because matrix is not square
-F = M(1:n, end-l+1:end);
-N = M(end-m+1:end, end-l+1:end);
-
 %% Simulink
 trailerIC = [3, 6]; %x_t y_t
-tractorIC = [trailerIC(1) + (lt+lh), trailerIC(2)]; 
+tractorIC = [trailerIC(1), trailerIC(2) + (lt+lh)]; 
 ICs = [deg2rad(0); 0; deg2rad(0); trailerIC(1,2)]; % phi_r phi_d_r phi_t y_t
-ur = deg2rad(30); %[deg/s]
+
 sim('LQRTrailerKinematics.slx')
+
+% y_te = OptimalControl(:,1);
+% phi_te = OptimalControl(:,2);
 
 y_te = physics(:,1);
 phi_te = physics(:,2);
@@ -94,8 +90,40 @@ phi_te = physics(:,2);
 figure
 subplot 211
 plot(tout, y_te)
+hold on
+plot(tout, xform_set(:,1), '--r')
+hold off
 ylabel('y_{t} [m]')
 subplot 212
 plot(tout, rad2deg(phi_te))
 ylabel('\phi_{t} [{\circ}]')
 xlabel('time [s]')
+movegui('west')
+
+figure
+hold on
+plot(trailer_xy(:,1), trailer_xy(:,2), 'b')
+plot(tractor_xy(:,1), tractor_xy(:,2), 'g')
+% x_center = 6;
+% y_center = -6;
+% t = 0:.01:2*pi;
+% x_desired = Radius*cos(t) + x_center;
+% y_desired = Radius*sin(t) + y_center;
+% plot(x_desired, y_desired, '--r')
+
+x_desired_line = linspace(trailerIC(1), tractor_xy(end,1), 100);
+y_desired_line = 0*ones(length(x_desired_line));
+plot(x_desired_line, y_desired_line, '--r')
+plot(trailer_xy(1,1), trailer_xy(1,2), 'ob')
+plot(tractor_xy(1,1), tractor_xy(1,2), 'og')
+plot(trailer_xy(end,1), trailer_xy(end,2), 'xb')
+plot(tractor_xy(end,1), tractor_xy(end,2), 'xg')
+axis square
+axis equal
+% ylim([-15 3])
+% xlim([-3 15])
+xlabel('Position in x [m]')
+ylabel('Position in y [m]')
+legend('trailer path', 'tractor path', 'desired path')
+hold off
+movegui('east')
