7e090caf981c1c8a6395ec3f832983be92aa29a6
journeyman9 <journey.e.mcdowell@gmail.com>
2018-08-10 17:53:17 -0700

    Built controller as regulator with linearized plant for the augmented model and it didn't work well, even as full state. Tried steering model as input and this worked! Also confirmed nonlinear plant works with curvature as input open loop.

--- trailerKinematicsLQR.m
+++ trailerKinematicsLQR.m
@@ -8,7 +8,7 @@ lr = 1.96; %[m] tractor wheelbase
 lt = 4; %[m] trailer wheelbase
 lh = 0.53; %[m] hitch wheelbase
 vr = 4.5; %[m/s] keep below 4.5 m/s
-orientation = 0; % 0 for Horizontal, 1 for vertical
+orientation = 1; % 0 for Horizontal, 1 for vertical
 
 tractorParams = [lr lt lh vr];
 
@@ -20,21 +20,38 @@ tractorParams = [lr lt lh vr];
 % vt = vr*cos(thss) + lh*wss*sin(thss);
 
 %% Linearized State Space
-A = [0          1           0           0;
-     0          0           0           0;
-     vr./lt     -lh./lt     -vr./lt     0;
-     0          0           vr          0];
+%Steering rate
+% A = [0          1           0           0;
+%      0          0           0           0;
+%      vr./lt     -lh./lt     -vr./lt     0;
+%      0          0           vr          0];
+%  
+% B = [0;
+%      vr./lr;
+%      0;
+%      0];
  
-B = [0;
-     vr./lr;
-     0;
+% C = [0 0 0 1
+%      0 0 1 0];
+%  
+% D = [0;
+%      0];
+
+% full state
+% C = eye(4);
+% D = zeros(4, 1);
+
+% steering angle
+A = [0       0         0;
+     vr./lt  -vr./lt   0;
+     0       vr        0];
+
+B = [vr./lt;
+     -lh*vr ./ (lr*lt);
      0];
- 
-C = [0 0 0 1
-     0 0 1 0]; 
- 
-D = [0;
-     0];
+
+C = eye(3);
+D = zeros(3, 1);
 
 sys = ss(A, B, C, D);
 
@@ -50,17 +67,26 @@ controllability = rank(ctrb(A, B));
 observability = rank(obsv(A, C));
 
 %% LQR Gains
-G = [0 0 0 0;
-     0 0 0 0;
-     0 0 0 1;
-     0 0 1 0];
-H = zeros(4,1);
+% %steering rate
+% G = [0 0 0 0;
+%      0 0 0 0;
+%      0 0 0 1;
+%      0 0 1 0];
+% H = zeros(4,1);
+% rho = 1;
+% R = 1;
+% Q = [0 0 0 0;
+%      0 0 0 0;
+%      0 0 4 0;
+%      0 0 0 3];
+
+%steering angle
+G = eye(3);
+H = zeros(3, 1);
 rho = 1;
 R = 1;
-Q = [0 0 0 0;
-     0 0 0 0;
-     0 0 4 0;
-     0 0 0 3];
+Q = eye(3);
+
 QQ = G'*Q*G;
 RR = H'*Q*H + rho*R;
 NN = G'*Q*H;
@@ -69,46 +95,66 @@ RN = eye(2); %Match dimension of output
 Bbar = B;
 
 [K S e] = lqr(sys, QQ, RR, NN);
-[est, L, P] = kalman(ss(A, [B Bbar], C, zeros(2,2)), QN, RN);
+% [est, L, P] = kalman(ss(A, [B Bbar], C, zeros(2,2)), QN, RN);
 
 %% Set Point Control
-Q_sp = [A, B; G, H];
-[n, n] = size(A);
-% [l, p] = size(C); %Number of controlled outputs
-l = 2;
-m = 1; %Number of process inputs, or just inputs
-M = pinv(Q_sp); %psuedo inverse because matrix is not square
-F = M(1:n, end-l+1:end);
-N = M(end-m+1:end, end-l+1:end);
+% Q_sp = [A, B; G, H];
+% [n, n] = size(A);
+% % [l, p] = size(C); %Number of controlled outputs
+% l = 2;
+% m = 1; %Number of process inputs, or just inputs
+% M = pinv(Q_sp); %psuedo inverse because matrix is not square
+% F = M(1:n, end-l+1:end);
+% N = M(end-m+1:end, end-l+1:end);
+
+%% Feedforward
+track_vector = csvread('t_circle.txt');
+s = track_vector(:, 5);
+t = abs(s / vr);
+curv = [t track_vector(:, 3)];
+yaw_r = [t track_vector(:, 4)];
+y_r = [t track_vector(:, 2)];
+x_r = [t track_vector(:, 1)];
+
+sim_time = t(end, 1);
 
 %% Simulink
-trailerIC = [0, 0.5]; %x_t y_t
+y_IC = 1;
 
 if orientation == 1
+    trailerIC = [track_vector(1,1)-y_IC*sin(90), track_vector(1, 2)+y_IC*cos(90)]; %x_t y_t
+    % steering rate
+%     tractorIC = [trailerIC(1), trailerIC(2) + (lt+lh)];
+%     ICs = [deg2rad(90); 0; deg2rad(90); y_IC]; %phi_r phi_d_r phi_t y_t
+    % steering angle
     tractorIC = [trailerIC(1), trailerIC(2) + (lt+lh)];
-    ICs = [deg2rad(90); 0; deg2rad(90); trailerIC(1, 2)]; %phi_r phi_d_r phi_t y_t
+    ICs = [deg2rad(90); deg2rad(90); y_IC]; %phi_r phi_t y_t
 else
+    % steering angle
+    trailerIC = [track_vector(1,1)-y_IC*sin(0), track_vector(1, 2)+y_IC*cos(0)]; %x_t y_t
+%     tractorIC = [trailerIC(1) + (lt+lh), trailerIC(2)]; 
+%     ICs = [deg2rad(0); 0; deg2rad(0); y_IC]; %phi_r phi_d_r phi_t y_t
     tractorIC = [trailerIC(1) + (lt+lh), trailerIC(2)]; 
-    ICs = [deg2rad(0); 0; deg2rad(0); trailerIC(1, 2)]; %phi_r phi_d_r phi_t y_t
+    ICs = [deg2rad(0); deg2rad(0); y_IC]; %phi_r phi_t y_t
 end
 
 sim('LQRTrailerKinematics.slx')
 
-y_te = output(:,1);
-phi_te = output(:,2);
+y_te = error(:,1);
+phi_te = error(:,2);
 
 %% Plots
 figure
 subplot 211
 plot(tout, y_te)
 hold on
-plot(tout, y_d, '--r')
+plot(tout, 0*linspace(0, length(tout), length(tout))', '--r')
 hold off
-ylabel('y_{t} [m]')
+ylabel('y_{te} [m]')
 subplot 212
 plot(tout, rad2deg(phi_te))
 hold on
-plot(tout, psi_d, '--r')
+plot(tout, 0*linspace(0, length(tout), length(tout))', '--r')
 hold off
 ylabel('\phi_{t} [{\circ}]')
 xlabel('time [s]')
@@ -116,32 +162,16 @@ legend('response', 'desired')
 
 % figure
 % hold on
-% plot(trailer_xy(:,1), trailer_xy(:,2), 'b')
-% plot(tractor_xy(:,1), tractor_xy(:,2), 'g')
-% % x_center = 6;
-% % y_center = -6;
-% % t = 0:.01:2*pi;
-% % x_desired = Radius*cos(t) + x_center;
-% % y_desired = Radius*sin(t) + y_center;
-% % plot(x_desired, y_desired, '--r')
+% plot(odometry(:, 5), odometry(:, 4), 'b') % trailer
+% plot(odometry(:, 7), odometry(:, 6), 'g') % tractor
 % 
-% x_desired_line = linspace(trailerIC(1), tractor_xy(end,1), 100);
-% y_desired_line = 0*ones(length(x_desired_line));
-% plot(x_desired_line, y_desired_line, '--r')
-% plot(trailer_xy(1,1), trailer_xy(1,2), 'ob')
-% plot(tractor_xy(1,1), tractor_xy(1,2), 'og')
-% plot(trailer_xy(end,1), trailer_xy(end,2), 'xb')
-% plot(tractor_xy(end,1), tractor_xy(end,2), 'xg')
+% plot(odometry(1, 5), odometry(1, 4), 'ob')
+% plot(odometry(1, 7), odometry(1, 6), 'og')
+% plot(odometry(end, 5), odometry(end, 4), 'xb')
+% plot(odometry(end, 7), odometry(end, 6), 'xg')
 % axis square
 % axis equal
-% % ylim([-15 3])
-% % xlim([-3 15])
 % xlabel('Position in x [m]')
 % ylabel('Position in y [m]')
 % legend('trailer path', 'tractor path', 'desired path')
 % hold off
-% 
-% figure
-% plot(tout, y_te)
-% hold on
-% plot(tout, trailer_xy(:,2))
