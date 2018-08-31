e87fe50112e07284198eff239b32eb5a0a96ded4
journeyman9 <journey.e.mcdowell@gmail.com>
2018-08-31 13:35:48 -0700

    New nonlinear plant works with curvature as input open loop. Trying to output y_trailer even though A = 3x3. LQR cannot calculate gains to control 4 states.

--- trailerKinematicsLQR.m
+++ trailerKinematicsLQR.m
@@ -4,25 +4,30 @@
 clear; close all; clc;
 
 %% Parameters
-lr = 1.96; %[m] tractor wheelbase
-lt = 4; %[m] trailer wheelbase
-lh = 0.53; %[m] hitch wheelbase
-vr = 4.5; %[m/s] keep below 4.5 m/s
+L1 = 1.96; %[m] tractor wheelbase
+L2 = 4; %[m] trailer wheelbase
+Lh = 0.53; %[m] hitch length
+v_x1 = 4.5; %[m/s] keep below 4.5 m/s
 orientation = 'up'; % right for horizontal, up for vertical, left for pi, and down for 3pi/2
 
-tractorParams = [lr lt lh vr];
-
 %% Linearized State Space
-A = [0       0         0;
-     vr./lt  -vr./lt   0;
-     0       vr        0];
+phi_eq = 0;
+del_eq = 0;
 
-B = [vr./lt;
-     -lh*vr ./ (lr*lt);
-     0];
+A = [0       v_x1        0;
+     0       0           0;
+     0       0  -(v_x1 * cos(phi_eq) / L2)];
 
-C = eye(3);
-D = zeros(3, 1);
+B = [0;
+     v_x1 / (L1 * (cos(del_eq)).^2);
+     v_x1 / (L1 * (cos(del_eq)).^2)];
+
+C = [1     0     0;
+     0     1     0;
+     0     0     1;
+     1 -(Lh+L2) L2];
+
+D = zeros(4, 1);
 
 sys = ss(A, B, C, D);
 
@@ -38,11 +43,11 @@ controllability = rank(ctrb(A, B));
 observability = rank(obsv(A, C));
 
 %% LQR Gains
-G = eye(3);
-H = zeros(3, 1);
+G = eye(4);
+H = zeros(4, 1);
 rho = 1;
 R = 1;
-Q = eye(3);
+Q = eye(4);
 
 QQ = G'*Q*G;
 RR = H'*Q*H + rho*R;
@@ -67,74 +72,93 @@ Bbar = B;
 %% Feedforward
 track_vector = csvread('t_circle.txt');
 s = track_vector(:, 5);
-t = abs(s / vr);
+t = abs(s / v_x1);
 curv = [t track_vector(:, 3)];
-yaw_trailer = [t track_vector(:, 4)];
-yaw_tractor = yaw_trailer;
+hitch = [t track_vector(:, 4)];
+yaw_tractor = [t track_vector(:, 4)];
 y_r = [t track_vector(:, 2)];
 x_r = [t track_vector(:, 1)];
 
 sim_time = t(end, 1);
 
 %% Simulink
-y_IC = 1;
+y_IC = 0;
 
 switch orientation
     case 'right'
-        trailerIC = [track_vector(1,1)-y_IC*sin(0), track_vector(1, 2)+y_IC*cos(0)]; %x_t y_t
-        tractorIC = [trailerIC(1) + (lt+lh), trailerIC(2)]; 
-        ICs = [deg2rad(0); deg2rad(0); y_IC]; %phi_r phi_t y_t
+        trailerIC = [track_vector(1,1)-y_IC*sin(0), track_vector(1, 2)+y_IC*cos(0), deg2rad(0)]; %x, y, yaw_trailer
+        tractorIC = [trailerIC(1) + (L2+Lh), trailerIC(2)]; %x, y
+        ICs = [y_IC; deg2rad(0); deg2rad(0)]; %y, yaw_tractor, hitch
     case 'up'
-        trailerIC = [track_vector(1,1)-y_IC*sin(pi/2), track_vector(1, 2)+y_IC*cos(pi/2)]; %x_t y_t
-        tractorIC = [trailerIC(1), trailerIC(2) + (lt+lh)];
-        ICs = [deg2rad(90); deg2rad(90); y_IC]; %phi_r phi_t y_t
+        trailerIC = [track_vector(1,1)-y_IC*sin(pi/2), track_vector(1, 2)+y_IC*cos(pi/2), deg2rad(90)];  %x, y, yaw_trailer
+        tractorIC = [trailerIC(1), trailerIC(2) + (L2+Lh)]; %x, y
+        ICs = [y_IC; deg2rad(90); deg2rad(0)]; %y, yaw_tractor, hitch
     case 'left'
-        trailerIC = [track_vector(1,1)-y_IC*sin(pi), track_vector(1, 2)+y_IC*cos(pi)]; %x_t y_t
-        tractorIC = [trailerIC(1) - (lt+lh), trailerIC(2)]; 
-        ICs = [deg2rad(180); deg2rad(180); y_IC]; %phi_r phi_t y_t
+        trailerIC = [track_vector(1,1)-y_IC*sin(pi), track_vector(1, 2)+y_IC*cos(pi), deg2rad(180)];  %x, y, yaw_trailer
+        tractorIC = [trailerIC(1) - (L2+Lh), trailerIC(2)]; %x, y
+        ICs = [y_IC; deg2rad(180); deg2rad(0)]; %y, yaw_tractor, hitch
     case 'down'
-        trailerIC = [track_vector(1,1)-y_IC*sin(pi/2), track_vector(1, 2)+y_IC*cos(pi/2)]; %x_t y_t
-        tractorIC = [trailerIC(1), trailerIC(2) - (lt+lh)];
-        ICs = [deg2rad(270); deg2rad(270); y_IC]; %phi_r phi_t y_t
+        trailerIC = [track_vector(1,1)-y_IC*sin(3*pi/2), track_vector(1, 2)+y_IC*cos(3*pi/2), deg2rad(270)];  %x, y, yaw_trailer
+        tractorIC = [trailerIC(1), trailerIC(2) - (L2+Lh)]; %x, y
+        ICs = [y_IC; deg2rad(270); deg2rad(0)]; %y, yaw_tractor, hitch
 end
 
 sim('LQRTrailerKinematics.slx')
 
-y_te = error(:,1);
-phi_te = error(:,2);
-
 %% Plots
-figure
-ax1 = subplot(2, 1, 1);
-plot(tout, y_te)
-hold on
-plot(tout, 0*linspace(0, length(tout), length(tout))', '--r')
-hold off
-ylabel('y_{te} [m]')
-ax2 = subplot(2, 1, 2);
-plot(tout, rad2deg(phi_te))
-hold on
-plot(tout, 0*linspace(0, length(tout), length(tout))', '--r')
-hold off
-ylabel('\psi_{te} [{\circ}]')
-xlabel('time [s]')
-legend('response', 'desired')
-movegui('west')
-linkaxes([ax1 ax2], 'x')
-
 figure
 hold on
 plot(track_vector(:, 1), track_vector(:, 2), '--r')
-plot(odometry(:, 5), odometry(:, 4), 'b') % trailer
-plot(odometry(:, 7), odometry(:, 6), 'g') % tractor
+plot(odometry(:, 6), odometry(:, 5), 'b') % trailer
+plot(odometry(:, 4), odometry(:, 3), 'g') % tractor
 
-plot(odometry(1, 5), odometry(1, 4), 'ob')
-plot(odometry(1, 7), odometry(1, 6), 'og')
-plot(odometry(end, 5), odometry(end, 4), 'xb')
-plot(odometry(end, 7), odometry(end, 6), 'xg')
+plot(odometry(1, 6), odometry(1, 5), 'ob')
+plot(odometry(1, 4), odometry(1, 3), 'og')
+plot(odometry(end, 6), odometry(end, 5), 'xb')
+plot(odometry(end, 4), odometry(end, 3), 'xg')
 axis square
 axis equal
 xlabel('Position in x [m]')
 ylabel('Position in y [m]')
 legend('desired path', 'trailer path', 'tractor path')
 hold off
+
+y_tractor_e = error(:,1);
+psi_1_e = error(:,2);
+phi_e = error(:, 3);
+y_trailer_e = error(:, 4);
+
+figure
+ax1 = subplot(4, 1, 1);
+plot(tout, y_tractor_e)
+hold on
+plot(tout, 0*linspace(0, length(tout), length(tout))', '--r')
+hold off
+ylabel('y_{tractor_e} [m]')
+
+ax2 = subplot(4, 1, 2);
+plot(tout, rad2deg(psi_1_e))
+hold on
+plot(tout, 0*linspace(0, length(tout), length(tout))', '--r')
+hold off
+ylabel('\psi_{1_e} [{\circ}]')
+
+ax3 = subplot(4, 1, 3);
+plot(tout, rad2deg(phi_e));
+hold on
+plot(tout, 0*linspace(0, length(tout), length(tout))', '--r')
+hold off
+ylabel('\phi_{e} [{\circ}]')
+
+ax4 = subplot(4, 1, 4);
+plot(tout, y_trailer_e)
+hold on
+plot(tout, 0*linspace(0, length(tout), length(tout))', '--r')
+hold off
+ylabel('y_{trailer_e} [m]')
+
+xlabel('time [s]')
+legend('response', 'desired')
+movegui('west')
+linkaxes([ax1 ax2], 'x')
+
